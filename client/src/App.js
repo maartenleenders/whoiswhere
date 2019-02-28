5876552b6ddd8ea702e6fbe2c8c5dc01c211f5b5
Alexander Botteram <a.botteram@gmail.com>
2019-02-28 14:33:08 +0100

    Initial app

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  state = { users: [] };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { this.state.users.map( user => (
+            <span>{ user.firstName + " " + user.lastName }</span>
+        ) ) }
       </div>
     );
+  }
+
+  componentDidMount() {
+    fetch( "http://localhost:3000/user" ).then( res => res.json() ).then( users => {
+      console.log( users );
+      this.setState( { users } );
+    } );
   }
 }
 
