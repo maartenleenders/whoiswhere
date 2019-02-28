3841e3fa2cbdbaff67aa8c9f5f0f1da993115d2e
Maarten Leenders <maarten@yoast.com>
2019-02-28 10:33:09 +0100

    Adding users employees and buildings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BuildingsController } from './buildings/buildings.controller';
+import { EmployeesController } from './employees/employees.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BuildingsController, EmployeesController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
