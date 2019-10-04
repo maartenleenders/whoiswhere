db1cfa792c4a0c14eaa1ea9681c0f8825582489a
Maarten Leenders <maarten@yoast.com>
2019-10-04 11:39:27 +0200

    fix deploy-to-docker script and add initial (non working) docker-compose file

--- scripts/deploy-to-docker.sh
+++ scripts/deploy-to-docker.sh
@@ -3,4 +3,6 @@
 docker build -t zeepje/whoiswhere .
 docker container stop whoiswhere
 docker container rm whoiswhere
-docker run -p 1337:3000 -d --name whoiswhere --network="whoiswhere" zeepje/whoiswhere
+docker network create whoiswhere
+docker run --network=whoiswhere --name whoiswhere-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=whoiswhere -d mysql:5
+docker run -p 1337:3000 -d --network="whoiswhere" --name whoiswhere-src zeepje/whoiswhere
