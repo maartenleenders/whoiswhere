04a38a6e4dc6d0843cff4894e89acf9b2e21b532
Maarten Leenders <maarten@yoast.com>
2020-05-22 19:33:57 +0200

    add process env port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 
 bootstrap();
